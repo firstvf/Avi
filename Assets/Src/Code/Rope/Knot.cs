2967d117d99f6c19da7203b781cc74c928ecbe64
firstvf <101809257+firstvf@users.noreply.github.com>
2025-01-18 19:58:14 +0300

    init

---
+++ Assets/Src/Code/Rope/Knot.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Src.Code.Rope
+{
+    public class Knot : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+    {
+        [SerializeField] private GameObject _shadow;
+        [SerializeField] private Rope _rope;
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            _shadow.gameObject.SetActive(true);
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            _shadow.gameObject.SetActive(false);
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            transform.position = eventData.position;
+            _rope.StretchRope();
+        }
+    }
+}
---
+++ Assets/Src/Code/Rope/Rope.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Src.Code.Rope
+{
+    public class Rope : MonoBehaviour
+    {
+        [SerializeField] private Sprite _greenRope, _redRope;
+        [SerializeField] private Knot[] _knot;
+        private Image _ropeImage;
+
+        private void Awake()
+        {
+            _ropeImage = GetComponent<Image>();
+        }
+
+        private void Start()
+        {
+            StretchRope();
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            Debug.Log(collision);
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            Debug.Log(collision);
+        }
+
+        public void StretchRope()
+        {
+            Debug.Log("stretch");
+            float distance = Vector2.Distance(_knot[0].transform.position, _knot[1].transform.position) / 100;
+
+            transform.localScale = new Vector3(distance, transform.localScale.y, transform.localScale.z);
+
+            Vector2 direction = _knot[1].transform.position - _knot[0].transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.SetPositionAndRotation((_knot[0].transform.position + _knot[1].transform.position) / 2, Quaternion.Euler(0, 0, angle));
+        }
+    }
+}
