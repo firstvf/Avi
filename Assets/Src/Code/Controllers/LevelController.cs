11ef4d78281b5c37212e30a359f5397964aa0d37
firstvf <101809257+firstvf@users.noreply.github.com>
2025-01-19 14:32:48 +0300

    upd

---
+++ Assets/Src/Code/Controllers/LevelController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets.Src.Code.Controllers
+{
+    public class LevelController : MonoBehaviour
+    {
+        public static LevelController Instance { get; private set; }
+        public int Score { get; private set; }
+        //public List<Rope.Rope> _ropeList
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                return;
+            }
+
+            Destroy(gameObject);
+        }
+
+
+    }
+}
